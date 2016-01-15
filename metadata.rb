4b0da6010f13c214d6b69533181a487910d878d0
Sascha Korth <sascha.korth@zweitag.de>
2016-01-15 11:32:14 +0100

    Changes base on https://github.com/chef-cookbooks/passenger_apache2/pull/40

--- metadata.rb
+++ metadata.rb
@@ -12,6 +12,8 @@ recipe 'passenger_apache2::mod_rails', 'Enables Apache module configuration for 
 depends 'apache2', '>= 2.0'
 depends 'build-essential'
 
+suggests "chef-rvm"
+
 %w( fedora redhat centos scientific amazon oracle ubuntu debian arch suse ).each do |os|
   supports os
 end
